048a22811124fbe8b89f5105819d1477c7c74127
ogie Ben <bensoft2k5@gmail.com>
2017-11-06 20:21:17 +0100

    Server Fixed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
@@ -7,18 +8,18 @@ app.use(express.static(__dirname+'/dist'));
 // Start the app by listening on the default
 // Heroku port
 
+ var val = path.join( __dirname+'/dist/index.html');
+app.get('/*', function(req, res) {
+  res.json({
+    path: val
+  });
+  // res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
 var port = 6010;
 app.listen(process.env.PORT || port, function(){
     console.log("Server started at port "+ port);
 });
-
-// var val = path.join( __dirname+'/dist/index.html');
-app.get('/*', function(req, res) {
-    res.json({
-        path: val
-    });
-    // res.sendFile(path.join(__dirname+'/dist/index.html'));
-  });
 
 
 /* 
