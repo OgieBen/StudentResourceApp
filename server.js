e0c40d85e867aa59d48b8a25f2e4f2cd0e525064
ogie Ben <bensoft2k5@gmail.com>
2017-11-06 20:08:02 +0100

    Fixed server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname+'/dist.'));
+app.use(express.static(__dirname+'/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080, function(){
