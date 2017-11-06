e3be2ee33f3520f08b34d83c1dec087e8446333d
ogie Ben <bensoft2k5@gmail.com>
2017-11-06 20:28:44 +0100

    SF

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname+'/dist'));
+//app.use(express.static(__dirname+'/dist'));
 // Start the app by listening on the default
 // Heroku port
 
