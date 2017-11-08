7d62d9a55588a45a64319c2a41314f6a090b25fa
ogie Ben <bensoft2k5@gmail.com>
2017-11-08 21:46:51 +0100

    UI update

--- src/app/view-resources/view-resources.component.ts
+++ src/app/view-resources/view-resources.component.ts
@@ -6,46 +6,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-view-resources',
-  //templateUrl: './view-resources.component.html',
-  template: `
-
-<div class="wrapper">
-
-  <div class="screen">
-
-  
-  <div class="loader-position centralize-vertical centralize-content" [class.display-loader]="!loader">
-  
-              <i class="fa fa-spinner fa-spin" style="font-size:62px; color: #004;"></i>
-              <!-- <i class="fa fa-circle-o-notch fa-spin" style="font-size:62px; color: #004;"></i> -->
-  
-     </div>
-
-      <div class=" col-xs-12 col-sm-12 col-md-12 col-lg-12 detail-wrapper">
-       <div class="row">
-          <div class="col-xs-9 col-sm-4 col-md-4 col-lg-2 dim-shadow detail-view"  *ngFor ="let resource of resources"  >
-          
-              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 resource-title-box">
-                <p class="resource-title">{{resource.title}}</p>
-              </div>
-
-              <div class=" col-xs-12 col-sm-12 col-md-12 col-lg-12 resource-bottom-box no-padding">
-              
-                  <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 author-box no-padding">
-                
-                      <p class="col-xs-12 col-sm-12 col-md-12 col-lg-12  author-name">Author: Ogie Ben</p>
-                      <button >delete</button>
-                  </div>
-                
-              </div>
-           </div>
-          </div>
-        
-
-      </div>
-    </div>
-    </div>
-  `,
+  templateUrl: './view-resources.component.html',
   styleUrls: ['./view-resources.component.css']
 })
 export class ViewResourcesComponent implements OnInit {
@@ -54,12 +15,16 @@ export class ViewResourcesComponent implements OnInit {
 
   tempStudents: any = [];
   loader = true;
+  deleteIcon: string;
+  infoIcon: string;
 
 
 
   constructor(
     private dataService: DataService,
     private httpClient: HttpClient) {
+    this.deleteIcon = '/assets/images/deleteIcon2.png';
+    this.infoIcon = '/assets/images/infoIcon.png';
 
   }
 
